c029f0203d91eb6513ebb8a602dc8a9d8b4b45ed
Daniel Jimenez <danieljimenez@outlook.co.nz>
2017-09-11 18:15:22 +1200

    parse and empty now working correctly.

--- tinyjsonpp.cpp
+++ tinyjsonpp.cpp
@@ -6,18 +6,23 @@
  */ 
 
 #include <avr/io.h>
+#include <string.h>
 #include "tinyjsonpp.h"
 
 tinyjsonpp::tinyjsonpp(bool store, unsigned int maxSize) {
+	char* newchar = static_cast<char*>(malloc(maxSize));
+	this->json = newchar;
 
+	this->jsonSize = maxSize;
+	this->location = 0;
 }
 
 tinyjsonpp::tinyjsonpp(bool store, bool ignoreQuotes, unsigned int maxSize) {
-	
-}
+	char* newchar = static_cast<char*>(malloc(maxSize));
+	this->json = newchar;
 
-void tinyjsonpp::parse(char* c) {
-
+	this->jsonSize = maxSize;
+	this->location = 0;
 }
 
 void* tinyjsonpp::operator new(size_t objsize) {
@@ -26,4 +31,20 @@ void* tinyjsonpp::operator new(size_t objsize) {
 
 void tinyjsonpp::operator delete(void* obj) {
 	free(obj);
+}
+
+void tinyjsonpp::parse(char c) {
+	// Ignore Newline, Space, Return Carriage Characters.
+	if (c <= 0x20) {
+		return;
+	}
+
+	this->json[this->location] = c;
+	++this->location;
+}
+
+void tinyjsonpp::empty() {
+	this->location = 0;
+	memset(this->json, 0, this->jsonSize);
+	// Alternative to save compute time is to just set the first character to 0.
 }
--- tinyjsonpp.h
+++ tinyjsonpp.h
@@ -17,15 +17,21 @@ public:
 	//         Variables
 	//------------------------------------------------------------------------------
 
-	unsigned int location; // The current location of the user. (Used by parse). (Enables user to insert K-V pairs into the current JSON string).
+	unsigned int location; // The current location of the user. (Used by parse). 
+						   // (Enables user to insert K-V pairs into the current 
+						   // JSON string).
 	char* json; // The JSON string.
+	unsigned int jsonSize; // The size of the json string.
 
-	char* key; // A pointer to the start of the key (ignoring quotation marks). This is used know where the user has read up to in the JSON string stored in memory. (next() uses this to the next key-value pair).
+	char* key; // A pointer to the start of the key (ignoring quotation marks). 
+			   // This is used know where the user has read up to in the JSON string
+			   // stored in memory. (next() uses this to the next key-value pair).
 	unsigned int keySize; // The amount of characters the key has.
 
 	char* value; // A pointer to the start of the value (ignoring quotation marks).
 	unsigned int valueSize; // The amount of characters the value has.
-	char valueType; // The value type (int => 'i', string => 's', char => 'c', bool => 'b', array => 'a').
+	char valueType; // The value type (int => 'i', string => 's', char => 'c', 
+					// bool => 'b', array => 'a').
 
 	//------------------------------------------------------------------------------
 	//         Methods
@@ -34,50 +40,77 @@ public:
 	/**
 	 * Constructor
 	 *
-	 * @param bool store			- Store in IRAM or in EEPROM (Depending on purpose). (1 = EEPROM, 0 = IRAM).
-	 * @param unsigned int maxSize	- Specify a maximum size that will be used by the JSON string (if known). If not chosen, the string will be of variable size (need to check to ensure string will be stored in a row).
+	 * @param bool store			- Store in IRAM or in EEPROM (Depending on 
+	 *								  purpose). (1 = EEPROM, 0 = IRAM).
+	 * @param unsigned int maxSize	- Specify a maximum size that will be used by the
+	 *								  JSON string (if known). If not chosen, the 
+	 *								  string will be of variable size (need to check 
+	 *								  to ensure string will be stored in a row).
 	 */
 	tinyjsonpp(bool store, unsigned int maxSize);
 
 	/**
 	 * Full Constructor
 	 *
-	 * @param bool store			- Store in IRAM or in EEPROM (Depending on purpose). (1 = EEPROM, 0 = IRAM).
-	 * @param bool ignoreQuotes		- Ignore quotation marks, saving space in memory. (To Be Implemented at a later date).
-	 * @param unsigned int maxSize	- Specify a maximum size that will be used by the JSON string (if known). If not chosen, the string will be of variable size (need to check to ensure string will be stored in a row).
+	 * @param bool store			- Store in IRAM or in EEPROM (Depending on
+	 *								  purpose). (1 = EEPROM, 0 = IRAM).
+	 * @param bool ignoreQuotes		- Ignore quotation marks, saving space in memory.
+	 *								  (To Be Implemented at a later date).
+	 * @param unsigned int maxSize	- Specify a maximum size that will be used by the
+	 *								  JSON string (if known). If not chosen, the 
+	 *								  string will be of variable size (need to check
+	 *								  to ensure string will be stored in a row).
 	 */
 	tinyjsonpp(bool store, bool ignoreQuotes, unsigned int maxSize);
 	
+	/** 
+	 * Construct New Object.
+	 *
+	 * @param size_t objsize		- The size of the tinyjsonpp object.
+	 * @return void
+	 */
+	void* operator new(size_t objsize);
+
+	/** 
+	 * Delete Object.
+	 *
+	 * @param void* obj				- The tinyjsonpp object to delete.
+	 * @return void
+	 */
+	void operator delete(void* obj);
+
 	/**
 	 * Read from buffer and store JSON string in either IRAM or EEPROM.
-	 * During the read, remove any whitespace, /r, /n characters to ensure the smallest possible stored JSON. (Potential to remove the quotation marks as well).
+	 * During the read, remove any whitespace, /r, /n characters to ensure the 
+	 * smallest possible stored JSON. (Potential to remove the quotation marks as well).
 	 * The character from the buffer will be added to the end of the JSON string.
 	 *
 	 * @param char* c				- The character currently being passed by the buffer.
-	 *
 	 * @return void
 	 */ 
-	void parse(char* c);
+	void parse(char c);
 
-	void* operator new(size_t objsize);
-
-	void operator delete(void* obj);
-
-
-
-	// User can also add key-value pairs to the JSON string after the current key-value pointer position (two variations, one function accepts a char* and the other accepts a key char*, value char*).
-	// This allows for users to provide an already formatted JSON key-value pair or just the key, value. (Unsure at this point as to how to handle array values/embedded key-value pairs).
+	// User can also add key-value pairs to the JSON string after the current key-value pointer position 
+	// (two variations, one function accepts a char* and the other accepts a key char*, value char*).
+	// This allows for users to provide an already formatted JSON key-value pair or just the key, value. 
+	// (Unsure at this point as to how to handle array values/embedded key-value pairs).
 
 	// user will loop through the  (will return a 1 if there is still JSON to parse, else 0. 'tinyjson.next()' function until the end of the JSON string has been reached.
 
-	// user can get current Key-Value Pair (depending on where next is) by looking at the 'tinyjsonpp.key' and 'tinyjsonpp.value' properties. They will also be able to look at the 'tinyjsonpp.valueType', which will
+	// user can get current Key-Value Pair (depending on where next is) by looking at the 'tinyjsonpp.key' and 'tinyjsonpp.value' properties. 
+	// They will also be able to look at the 'tinyjsonpp.valueType', which will
 	// indicate what type the characters represent. (int => 'i', string => 's', char => 'c', bool => 'b', array => 'a').
 
 	// value will always return a char *. (A Pointer to the start of value in the stored JSON string).
 
 	// There is also a function that will allow looping through the string one character at a time for use when transmitting UART
 
-	// Function to empty the current JSON string.
+	/** 
+	 * Empties the current JSON string.
+	 *
+	 * @return void
+	 */
+	void empty();
 };
 
 #endif /* LIBRARY_H_ */
