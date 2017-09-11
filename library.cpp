073f0f364428cee259ebc5438584230f54c635f2
Daniel Jimenez <danieljimenez@outlook.co.nz>
2017-09-11 12:30:59 +1200

    Inital Commit.

---
+++ library.cpp
@@ -0,0 +1,16 @@
+/*
+ * TinyJSONpp.cpp
+ *
+ * Created: 11-Sep-17 12:25:10
+ * Author : Dan
+ */ 
+
+#include <avr/io.h>
+
+
+/* Replace with your library code */
+int myfunc(void)
+{
+	return 0;
+}
+
